942a347e892efeee5354ed909b7b3a0f61da5e7e
hiro16131104 <hiro931104@gmail.com>
2025-01-22 00:34:55 +0900

    add: グラフのコピー機能、日付の指定機能を追加

--- script.js
+++ script.js
@@ -8,11 +8,21 @@ let state = {
 
 document.addEventListener("DOMContentLoaded", () => {
   const fileInput = document.querySelector("#fileInput");
+  const displayButton = document.querySelector("#displayButton");
+  const resetZoomButton = document.querySelector("#resetZoomButton");
+  const copyButton = document.querySelector("#copyButton");
+  const applyRangeButton = document.querySelector("#applyRangeButton");
+
   fileInput.addEventListener("change", handleChangeFileInput);
+  displayButton.addEventListener("click", handleClickDisplayButton);
+  resetZoomButton.addEventListener("click", handleClickResetZoomButton);
+  copyButton.addEventListener("click", handleClickCopyButton);
+  applyRangeButton.addEventListener("click", handleClickApplyRangeButton);
 });
 
 function handleChangeFileInput(event) {
-  const file = event.target.files[0];
+  const fileInput = event.target;
+  const file = fileInput.files[0];
 
   if (!file) {
     return;
@@ -22,14 +32,23 @@ function handleChangeFileInput(event) {
 
   if (!["xlsx", "xls"].includes(extension)) {
     alert("Excelファイルを選択してください。");
-    event.target.value = "";
+    fileInput.value = "";
     return;
   }
 
   const reader = new FileReader();
+  const buttonDiv = document.querySelector("#buttonDiv");
+  const resetZoomButton = document.querySelector("#resetZoomButton");
+  const copyButton = document.querySelector("#copyButton");
+  const rangeDiv = document.querySelector("#rangeDiv");
 
   reader.addEventListener("load", handleLoadFileReader);
   reader.readAsArrayBuffer(file);
+  buttonDiv.classList.remove("hidden");
+  displayButton.textContent = "グラフを表示";
+  resetZoomButton.disabled = true;
+  copyButton.disabled = true;
+  rangeDiv.classList.add("hidden");
 }
 
 function handleLoadFileReader(event) {
@@ -39,24 +58,21 @@ function handleLoadFileReader(event) {
   const nameDiv = document.querySelector("#nameDiv");
 
   nameDiv.innerHTML = "";
-  state = {
-    jsonData: null,
-    xAxis: "",
-    leftYAxis: [],
-    rightYAxis: [],
-    chart: null,
-  };
+  resetState();
   renderSettingTable(names);
-  renderDisplayButton();
   state.jsonData = jsonData;
 }
 
 function handleClickDisplayButton(event) {
+  const displayButton = event.target;
+  const resetZoomButton = document.querySelector("#resetZoomButton");
+  const copyButton = document.querySelector("#copyButton");
   const selectedRadio = document.querySelector('input[name="XAxis"]:checked');
   const checkedBoxes = {
     leftYAxis: document.querySelectorAll('input[name="leftYAxis"]:checked'),
     rightYAxis: document.querySelectorAll('input[name="rightYAxis"]:checked'),
   };
+  const rangeDiv = document.querySelector("#rangeDiv");
 
   state.xAxis = selectedRadio.value;
   state.leftYAxis = Array.from(checkedBoxes.leftYAxis).map((box) => box.value);
@@ -64,7 +80,66 @@ function handleClickDisplayButton(event) {
     (box) => box.value
   );
   createChart();
-  event.target.textContent = "グラフを更新 / ズームをリセット";
+  displayButton.textContent = "グラフを更新";
+  resetZoomButton.disabled = false;
+  copyButton.disabled = false;
+  rangeDiv.classList.remove("hidden");
+  setInitialDateRange(state.jsonData);
+}
+
+function handleClickResetZoomButton() {
+  state.chart.resetZoom();
+}
+
+function handleClickCopyButton() {
+  const canvas = document.querySelector("#chartCanvas");
+
+  canvas.toBlob((blob) => {
+    const item = new ClipboardItem({ "image/png": blob });
+
+    window.navigator.clipboard.write([item]).then(
+      () => {
+        alert("クリップボードにコピーしました");
+      },
+      (error) => {
+        console.error("コピーに失敗しました:", error);
+        alert("コピーに失敗しました");
+      }
+    );
+  });
+}
+
+function handleClickApplyRangeButton() {
+  const startDateInput = document.querySelector("#startDateInput");
+  const endDateInput = document.querySelector("#endDateInput");
+
+  if (!startDateInput.value || !endDateInput.value) {
+    alert("開始日時と終了日時が入力されていません");
+    return;
+  }
+
+  const startDate = new Date(startDateInput.value);
+  const endDate = new Date(endDateInput.value);
+
+  if (startDate > endDate) {
+    alert("開始日時が終了日時より後ろです");
+    return;
+  }
+
+  const filteredJsonData = state.jsonData.filter((item) => {
+    const itemDate = new Date(item[state.xAxis]);
+    return itemDate >= startDate && itemDate <= endDate;
+  });
+
+  if (filteredJsonData.length === 0) {
+    alert("指定された範囲にデータが存在しません");
+    return;
+  }
+  console.log(startDate);
+  console.log(endDate);
+  console.log(filteredJsonData);
+
+  updateChart(filteredJsonData);
 }
 
 function readExcelFile(inputElem) {
@@ -130,97 +205,120 @@ function renderSettingTable(names) {
   nameDiv.appendChild(table);
 }
 
-function renderDisplayButton() {
-  const nameDiv = document.querySelector("#nameDiv");
-  const button = document.createElement("button");
-
-  button.id = "displayButton";
-  button.className = "button margin-top cursor-pointer";
-  button.textContent = "グラフを表示";
-  button.addEventListener("click", handleClickDisplayButton);
-  nameDiv.appendChild(button);
-}
-
-function createChart() {
-  const labels = state.jsonData.map((item) =>
-    getDateTimeString(new Date(item[state.xAxis]))
-  );
+function renderChart(jsonData) {
   const datasets = [];
-  const ctx = document.querySelector("#chartCanvas").getContext("2d");
+  const scales = {};
   let count = 0;
 
-  for (const name of state.leftYAxis) {
-    const dataset = {
-      label: name,
-      data: state.jsonData.map((item) => item[name]),
-      borderColor: colors[count].borderColor,
-      backgroundColor: colors[count].backgroundColor,
-      yAxisID: "leftY",
-    };
+  if (state.leftYAxis.length !== 0) {
+    for (const name of state.leftYAxis) {
+      const dataset = {
+        label: name,
+        data: jsonData.map((item) => item[name]),
+        borderColor: colors[count].borderColor,
+        backgroundColor: colors[count].backgroundColor,
+        yAxisID: "leftY",
+      };
 
-    datasets.push(dataset);
-    count++;
+      datasets.push(dataset);
+      count++;
+    }
+
+    scales.leftY = { type: "linear", position: "left" };
   }
 
-  for (const name of state.rightYAxis) {
-    const dataset = {
-      label: name,
-      data: state.jsonData.map((item) => item[name]),
-      borderColor: colors[count].borderColor,
-      backgroundColor: colors[count].backgroundColor,
-      yAxisID: "rightY",
-    };
+  if (state.rightYAxis.length !== 0) {
+    for (const name of state.rightYAxis) {
+      const dataset = {
+        label: name,
+        data: jsonData.map((item) => item[name]),
+        borderColor: colors[count].borderColor,
+        backgroundColor: colors[count].backgroundColor,
+        yAxisID: "rightY",
+      };
 
-    datasets.push(dataset);
-    count++;
+      datasets.push(dataset);
+      count++;
+    }
+
+    scales.rightY = {
+      type: "linear",
+      position: "right",
+      grid: {
+        drawOnChartArea: false,
+      },
+    };
   }
+
+  const context = document.querySelector("#chartCanvas").getContext("2d");
+  const labels = jsonData.map((item) =>
+    getDateTimeString(new Date(item[state.xAxis]))
+  );
+
+  const config = {
+    type: "line",
+    data: { labels, datasets },
+    options: {
+      responsive: true,
+      scales,
+      plugins: {
+        zoom: {
+          pan: { enabled: true, mode: "x" },
+          zoom: {
+            wheel: { enabled: true },
+            pinch: { enabled: true },
+            mode: "x",
+          },
+        },
+      },
+    },
+  };
 
   if (state.chart) {
     state.chart.destroy();
   }
 
-  Chart.register(ChartZoom);
-  state.chart = new Chart(ctx, {
-    type: "line",
-    data: { labels, datasets },
-    options: {
-      responsive: true,
-      scales: {
-        leftY: {
-          type: "linear",
-          position: "left",
-        },
-        rightY: {
-          type: "linear",
-          position: "right",
-          grid: {
-            drawOnChartArea: false,
-          },
-        },
-      },
-      plugins: {
-        zoom: {
-          zoom: {
-            wheel: {
-              enabled: true,
-            },
-            pinch: {
-              enabled: true,
-            },
-            mode: "xy",
-          },
-        },
-      },
-    },
-  });
+  state.chart = new Chart(context, config);
 }
 
-function getDateTimeString(date) {
+function createChart() {
+  renderChart(state.jsonData);
+}
+
+function updateChart(filteredJsonData) {
+  renderChart(filteredJsonData);
+}
+
+function setInitialDateRange(jsonData) {
+  const xAxisData = jsonData.map((item) => new Date(item[state.xAxis]));
+  const minDate = new Date(Math.min(...xAxisData));
+  const maxDate = new Date(Math.max(...xAxisData));
+  const startDateInput = document.querySelector("#startDateInput");
+  const endDateInput = document.querySelector("#endDateInput");
+
+  startDateInput.value = getDateTimeString(minDate, true);
+  endDateInput.value = getDateTimeString(maxDate, true);
+}
+
+function getDateTimeString(date, iso8601 = false) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
 
-  return `${year}/${month}/${day} ${hours}:${minutes}`;
+  return iso8601
+    ? `${year}-${month}-${day}T${hours}:${minutes}`
+    : `${year}/${month}/${day} ${hours}:${minutes}`;
+}
+
+function resetState() {
+  state.jsonData = null;
+  state.xAxis = "";
+  state.leftYAxis = [];
+  state.rightYAxis = [];
+
+  if (state.chart) {
+    state.chart.destroy();
+  }
 }
